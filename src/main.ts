c4159ae8398412be92bb343d1694a0d8a29a4664
Chan Wei Qi <chanweiqi27@gmail.com>
2023-09-17 12:04:52 +0800

    feat: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.ALLOWED_CORS });
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(process.env.GLOBAL_PREFIX || 'api');
   app.useGlobalInterceptors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
